4c2c0aea5ecefa0260811d235761c91b414c7e2e
kumia01 <thien11hei@gmail.com>
2022-10-07 13:02:20 +0200

    adding models, controller

---
+++ ghostproject/Controllers/BrukerController.cs
@@ -0,0 +1,26 @@
+﻿using ghostproject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ghostproject.Controllers
+{
+
+    [Route("[controller]/[action]")]
+    public class BrukerController : ControllerBase
+    {
+        public List<Bruker> Bruker;
+        public bool Lagre (Bruker innBruker)
+        {
+
+            try
+            {
+                Bruker.Add(innBruker);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ghostproject/Models/Bruker.cs
@@ -0,0 +1,13 @@
+﻿namespace ghostproject.Models
+{
+    public class Bruker
+    {
+        public int Id { get; set; }
+        public string PersonNr { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public string Adresse { get; set; }
+        public string PostNr { get; set; }
+        public string Poststed { get; set; }
+    }
+}|
