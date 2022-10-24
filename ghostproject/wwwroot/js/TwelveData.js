3bf6185db7dda4eefe3da4bebe7dab9af0f5b978
Ivanna Ustymenko <s351877@oslomet.no>
2022-10-24 17:27:40 +0200

    fil for api integrasjon - ikke gerdig

---
+++ ghostproject/wwwroot/js/TwelveData.js
@@ -0,0 +1,3 @@
+﻿let TDKEY = "29c60ea7bebc433abb01eb0800ec1929";
+
+$.getJSON("https://api.twelvedata.com/price?symbol" + "&apikey=" + TDKEY, function(tickers));
--- ghostproject/wwwroot/js/index.js
+++ ghostproject/wwwroot/js/index.js
@@ -1,17 +1,1 @@
-﻿$(document).ready(function () {
-    $("#btnLogInn").click(function () {
-        window.location.href = 'html/loginn.html';
-    });
-});
-
-$(document).ready(function () {
-    $("#btnAksjeanalyse").click(function () {
-        window.location.href = 'html/aksjeanalyse.html';
-    });
-});
-
-$(document).ready(function () {
-    $("#btnEksperthjelp").click(function () {
-        window.location.href = '../html/eksperthjelp.html';
-    });
-});
+﻿
