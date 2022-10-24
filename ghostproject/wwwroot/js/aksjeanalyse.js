bd93a2d18d8e2a3d78edc8ec0b2c8aa178f5b7c8
ErsanSinani <72137313+ErsanSinani@users.noreply.github.com>
2022-10-24 23:35:07 +0200

    Noen retting på feil

--- ghostproject/wwwroot/js/aksjeanalyse.js
+++ ghostproject/wwwroot/js/aksjeanalyse.js
@@ -1,7 +1,13 @@
-﻿function hentAlleAksjer() {
+﻿$(funtion(){
+    hentAlleAksjer();
+});
+
+
+function hentAlleAksjer() {
 
 //Begynnelse på å liste alle aksjer i kundeanalyse
     $.get("aksje/hentAlle", function (aksjer) {
         formaterAksjer(aksjer);
     });
 }
+
