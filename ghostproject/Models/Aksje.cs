738c1987dd97f584ec8aaf002fd4322c8f70b21e
Spermulf <gjermundhertzberg@gmail.com>
2022-10-24 15:58:56 +0200

    TransaksjonController

--- ghostproject/Models/Aksje.cs
+++ ghostproject/Models/Aksje.cs
@@ -8,5 +8,6 @@ namespace ghostproject.Models
         public string Ticker { get; set; }
         public string Selskap { get; set; }
         public int Pris { get; set; }
+        virtual public List<Transaksjoner> Transaksjoner { get; set; }
     }
 }
--- ghostproject/wwwroot/js/admin.js
+++ ghostproject/wwwroot/js/admin.js
@@ -1,4 +1,41 @@
 ﻿$(document).ready(function () {
+
+    //const id = window.location.search.substring(1);
+    //const url = "../Bruker/HentEn?" + id;
+    
+    $("#id").val(1); // må ha med id inn skjemaet, hidden i html
+    $("#aksjeid").val(1);
+    
+
+    $("#btnSkriv").click(function () {
+        hentAlleTransaksjoner();
+    });
+});
+
+function hentAlleTransaksjoner() {
+    $.get("../Transaksjon/HentAlle", function (Transaksjon) {
+        formaterTransaksjon(Transaksjon);
+    });
+}
+
+function formaterTransaksjon(Transaksjon) {
+    let ut = "<table class='table table-striped'>" +
+        "<tr>" +
+        "<th>Volum</th><th>Pris</th><th></th><th></th>" +
+        "</tr>";
+    for (let transaksjon of Transaksjon) {
+        ut += "<tr>" +
+            "<td>" + transaksjon.volum + "</td>" +
+            "<td>" + transaksjon.pris + "</td>" +
+            "</tr>";
+    }
+    ut += "</table>";
+    $("#testTransaksjon").html(ut);
+}
+
+
+
+/*$(document).ready(function () {
     $("#btnSkriv").click(function () {
         hentAlleBrukere();
     });
@@ -8,7 +45,7 @@ function hentAlleBrukere() {
     $.get("../Bruker/HentAlle", function (Bruker) {
         formaterBruker(Bruker);
     });
-}
+}*/
 
 function formaterBruker(Bruker) {
     let ut = "<table class='table table-striped'>" +
@@ -45,5 +82,24 @@ function lagreBruker() {
                 $("#feil").html("Feil i db - prøv igjen senere");
             }
         });
+}
+
+function lagreTransaksjon() {
+    const transaksjon = {
+        Volum: $("#testVolum").val(),
+        Pris: $("#testPris").val(),
+        BrukereId: $("#id").val(),
+        AksjeId: $("#aksjeid").val()
+    }
+    const url = "../Transaksjon/Lagre";
+    console.log(transaksjon);
+    $.post(url, transaksjon, function (OK) {
+        if (OK) {
+            $("#feil").html("Lagt inn i db");
+        }
+        else {
+            $("#feil").html("Feil i db - prøv igjen senere")
+        }
+    });
 }
 
