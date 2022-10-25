70dd0725568d0b866505ed19fea0f803443704fa
ErsanSinani <ErsanSinani@users.noreply.github.com>
2022-10-25 14:01:52 +0200

    Skrevet litt mer på aksjeanalyse.JS

--- ghostproject/wwwroot/js/aksjeanalyse.js
+++ ghostproject/wwwroot/js/aksjeanalyse.js
@@ -10,4 +10,38 @@ function hentAlleAksjer() {
         formaterAksjer(aksjer);
     });
 }
+function formaterAksjer(Aksjer) {
+    let ut = "<table class='table table-striped'>" +
+        "<tr>" +
+        "<th>ID</th><th>Selskap</th><th>Ticker</th><th></th><th>Pris</th><th>Prisendring</th>" +
+        "</tr>";
+
+
+    for (let Aksje of Aksjer) {
+        int sum = (Aksje.gammelPris - Aksje.pris) / Aksje.pris
+
+        if (sum < 0) {
+
+            ut += "<tr>" +
+                "<td>" + Aksje.id + "</td>" +
+                "<td>" + Aksje.selskap + "</td>" +
+                "<td>" + Aksje.ticker + "</td>" +
+                "<td>" + Aksje.pris + "</td>" +
+                + "<td>" + sum + "</td>"
+            "</tr>";
+        } else {
+            ut += "<tr>" +
+                "<td>" + Aksje.id + "</td>" +
+                "<td>" + Aksje.selskap + "</td>" +
+                "<td>" + Aksje.ticker + "</td>" +
+                "<td>" + Aksje.pris + "</td>" +
+                + "<td>" + sum + "</td>"
+            "</tr>";
+
+        }
+    }
+    
+    ut += "</table>";
+    $("#testAksje").html(ut);
+}
 
