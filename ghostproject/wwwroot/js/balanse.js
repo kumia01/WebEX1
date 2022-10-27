f685eb3a160a1b595f0d90ef586151dfbea37a3c
Ivanna Ustymenko <s351877@oslomet.no>
2022-10-27 02:08:31 +0200

    chart for balanse

--- ghostproject/wwwroot/js/balanse.js
+++ ghostproject/wwwroot/js/balanse.js
@@ -1,1 +1,29 @@
-﻿
+﻿/*<script>
+    var xValues = ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Des"];
+    var yValues = [2500, 2400, 2300, 2400, 4350, 3700, 3250, 2000, 3000, 1500, 2000, 2000];
+
+    var balanse = new Chart("balanse", {
+        type: "line",
+    data: {
+        labels: xValues,
+    datasets: [{
+        fill: false,
+    tension: 0.2,
+    pointRadius: 0.5,
+    borderWidth: 2,
+    color: "#023e73",
+    backgroundColor: "#023e73",
+    borderColor: "#023e73",
+    hoverBorderColor: "#023e73",
+    pointHoverBackgroundColor: "#023e73",
+    data: yValues
+                        }]
+                    },
+    options: {
+        legend: {display: false },
+    scales: {
+        yAxes: [{ticks: {min: 0, max: 5000 } }],
+                        }
+                    }
+                });
+</script>*/
